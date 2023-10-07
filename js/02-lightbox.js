715e22c080d100037482b774ab0c5dead4a5e081
Vitalina28 <132218868+Vitalina28@users.noreply.github.com>
2023-10-07 20:51:23 +0200

    corrected js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const imageContainer = document.querySelector('.gallery');
+const imageMarkup = creatImageCardsMarkup(galleryItems);
+
+imageContainer.insertAdjacentHTML('beforeend', imageMarkup);
+
+
+function creatImageCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    }).join('');
+};
+
+
+  new SimpleLightbox('.gallery__item a', {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+
+  
